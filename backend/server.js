5e20f4b0059b60506322cb5d6ed597dafa16f3c3
Tara025 <tara_jones@web.de>
2023-08-17 22:43:29 +0200

    .

--- backend/server.js
+++ backend/server.js
@@ -37,3 +37,6 @@ const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`));
 
+app.use("/", express.static("./frontend"));
+app.get("/*", (req,res)=> res.sendFile("/frontend/index.html", {root: process.env.PWD}));
+
