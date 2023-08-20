69301c2d0a53300be01198afb4c7af9e05f398a7
Tara025 <tara_jones@web.de>
2023-08-20 19:11:32 +0200

    GlobalState Pfad geändert

--- backend/GlobalState.js
+++ backend/GlobalState.js
@@ -19,7 +19,7 @@ export const GlobalProvider = ({ children }) => {
   // Actions
   async function getTransactions() {
     try {
-      const res = await axios.get('/api/v1/transactions'); //render.com (für Backend) Verbindung HIER
+      const res = await axios.get('https://kassenbuch.onrender.com:5002/api/v1/transactions'); //render.com (für Backend) Verbindung HIER
 
       dispatch({
         type: 'GET_TRANSACTIONS',
@@ -35,7 +35,7 @@ export const GlobalProvider = ({ children }) => {
 
   async function deleteTransaction(id) {
     try {
-      await axios.delete(`/api/v1/transactions/${id}`);
+      await axios.delete(`https://kassenbuch.onrender.com:5002/api/v1/transactions/${id}`);
 
       dispatch({
         type: 'DELETE_TRANSACTION',
@@ -57,7 +57,7 @@ export const GlobalProvider = ({ children }) => {
     }
 
     try {
-      const res = await axios.post('/api/v1/transactions', transaction, config);
+      const res = await axios.post('https://kassenbuch.onrender.com:5002/api/v1/transactions', transaction, config);
 
       dispatch({
         type: 'ADD_TRANSACTION',
